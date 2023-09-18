db5aa868a5d88a7f7b4dc3328315ad0c500c35b3
Sarah E <seatherly.prsvr@gmail.com>
2023-09-18 16:34:23 -0500

    finished

--- main.js
+++ main.js
@@ -1,7 +1,45 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin(obj){
+    if(obj.userRole === 'ADMIN'){
+        return true;
+    }else{
+        return false;
+    }
+}
 
+function getEmail(obj){
+    return obj.firstName[0].toLowerCase() + obj.lastName.toLowerCase() + '.prsvr@gmail.com'
+}
+
+function getPlaylistLength(obj){
+    return obj.songs.length
+}
+
+function getHardestHomework(arr){
+    if(arr.length === 0){
+        return '';
+    }
+    let hardest = arr[0].name;
+    let lowestScore = arr[0].averageScore
+    for(let i=0; i < arr.length; i++){
+        if(arr[i].averageScore < lowestScore){
+            lowestScore = arr[i].averageScore;
+            hardest = arr[i].name
+        }
+
+    }
+    return hardest;
+}
+
+function createPhonebook(names, numbers){
+    let phonebook = {};
+   for(let i=0; i < names.length; i++){
+        phonebook[names[i]] = numbers[i]
+   }
+   return phonebook
+}
 
 
 // ┌─────────────────────────────────────┐
